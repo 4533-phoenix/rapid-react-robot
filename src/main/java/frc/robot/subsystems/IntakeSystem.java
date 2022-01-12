088300f13e7cb9517a49c8014d7a4c85dd7e8fb0
Sam Henshaw <57374015+sam-henshaw@users.noreply.github.com>
2022-01-12 17:19:44 -0500

    Created non-drive subsystems and command factories

---
+++ src/main/java/frc/robot/subsystems/IntakeSystem.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class IntakeSystem extends SubsystemBase {
+  public IntakeSystem() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
