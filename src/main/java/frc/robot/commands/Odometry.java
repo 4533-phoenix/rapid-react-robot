07d1c32af143f5954957e10c1e48408f86e2118c
Sam Henshaw <57374015+sam-henshaw@users.noreply.github.com>
2022-01-24 18:59:55 -0500

    Started work on odometry class

---
+++ src/main/java/frc/robot/commands/Odometry.java
@@ -0,0 +1,11 @@
+package frc.robot.commands;
+
+import edu.wpi.first.math.MathSharedStore;
+import edu.wpi.first.math.MathUsageId;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Twist2d;
+
+public class Odometry {
+    
+}
