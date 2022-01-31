7c8d0b2f7450acaa3f328566c91052114f490cf6
Bigman <89807949+Charlie-Es-Cool-and-Also-Guapo@users.noreply.github.com>
2022-01-31 17:55:50 -0500

    Added Two auto commands
    Added  driveDistanceAutoCommand and angularTurnAutoCommand

--- src/main/java/frc/robot/commands/AutoCommands.java
+++ src/main/java/frc/robot/commands/AutoCommands.java
@@ -9,5 +9,28 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Robot;
 
 public class AutoCommands {
-    
+
+    public AutoCommands() {
+    }
+        
+    public static Command driveDistanceAutoCommand(double distance, Direction direction) {
+        return new FunctionalCommand(
+            () -> Robot.drive.resetPosition(),
+            () -> Robot.drive.driveDistance(distance, direction),
+            (interrupt) -> Robot.drive.tank(0, 0),
+            () -> Robot.drive.reachedPosition(),
+            Robot.drive
+            );
+     }
+
+    public static Command angularTurnAutoCommand(double speed, double angle, Direction direction) {
+        return new FunctionalCommand(
+            () -> Robot.drive.resetAngle(),
+            () -> Robot.drive.turn(speed, direction),
+            (interrupt) -> Robot.drive.tank(0, 0),
+            () -> Robot.drive.getAngle() >= angle,
+            Robot.drive
+        );
+    }
+
 }
