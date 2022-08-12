a13e478054480b51ed312e9a160002a33f30be2b
BreadStixFTW <77415101+BreadStixFTW@users.noreply.github.com>
2022-08-11 22:50:48 -0400

    Created tests, added and edited documentation, and refactored some methods.
    Created tests for testing drive PID (issue #3), drive odometry (issue #4), shooter hood (issue #6), shooter vision (issue #5), and shooter auto shoot (issue #7) via creating a testing framework that runs the tests in test mode and allows for the editing of values in the wpilib Shuffleboard. Added and edited documentation (issue #2) for the new tests and new methods that the tests required of other subsystems. Mainly refactored the setPIDF() method that was located in Robot.java, which has now been moved to DriveSystem.java in the form of two overloaded methods, one that accepts a drive mode enum to set the PIDF values and one that directly accepts PIDF values.

---
+++ src/main/java/frc/robot/tests/DriveOdometryTest.java
@@ -0,0 +1,120 @@
+package frc.robot.tests;
+
+import edu.wpi.first.wpilibj.shuffleboard.*;
+import edu.wpi.first.util.sendable.Sendable.*;
+import edu.wpi.first.networktables.*;
+import edu.wpi.first.wpilibj2.command.*;
+import edu.wpi.first.math.geometry.Pose2d;
+
+import frc.robot.tests.Test;
+import frc.robot.subsystems.DriveSystem;
+import frc.robot.commands.AutoCommands;
+import frc.robot.Robot;
+import frc.robot.Constants;
+
+/**
+ * The class for testing drive odometry in Test mode.
+ */
+public class DriveOdometryTest extends Test {
+    /**
+     * The Shuffleboard tab that the odometry
+     * testing will be displayed on.
+     */
+    private ShuffleboardTab tab;
+
+    /**
+     * The Shuffleboard list that the odometry
+     * testing will be displayed in.
+     */
+    private ShuffleboardLayout layout;
+
+    /**
+     * The robot position for odometry testing.
+     */
+    private NetworkTableEntry robotPosition;
+
+    /**
+     * The robot angle for odometry testing.
+     */
+    private NetworkTableEntry robotAngle;
+
+    /**
+     * The target position for odometry testing.
+     */
+    private NetworkTableEntry targetPosition;
+
+    /**
+     * The value for whether or not odometry testing is enabled.
+     */
+    private NetworkTableEntry enableOdometry;
+
+    /**
+     * Stores the drive to position command 
+     * ({@link AutoCommands#driveToPosAutoCommand(double, double)}).
+     */
+    private Command command;
+
+    /**
+     * The constructor for our drive odometry test.
+     */
+    public DriveOdometryTest() {
+        this.tab = Shuffleboard.getTab("Test");
+        this.layout = tab.getLayout("Drive Odometry Testing").withPosition(2, 0).withSize(2, 4);
+
+        Pose2d robotPos = Robot.drive.getRobotPos();
+
+        this.robotPosition = layout.add("Robot Pos", new double[]{robotPos.getX(), robotPos.getY()}).getEntry();
+        this.robotAngle = layout.add("Robot Angle", Robot.drive.getRobotAngle()).getEntry();
+
+        /*
+         * Set the initial target position of the robot to the 
+         * robot's current position so that when the test is 
+         * enabled, it doesn't immediately drive to another
+         * location.
+         */
+        this.targetPosition = layout.add("Target Pos", new double[]{robotPos.getX(), robotPos.getY()}).getEntry();
+
+        this.enableOdometry = layout.add("Enable Odometry Test", false).withWidget(BuiltInWidgets.kToggleButton).getEntry();
+    }
+    
+    /**
+     * This method is run every loop of the Test periodic function in the 
+     * {@link Robot Robot class}.
+     * <p>
+     * It updates the current robot position and angle, it
+     * checks what the current target position is, and it 
+     * schedules for the robot to drive to that target position 
+     * if a command for the robot to drive to that target position 
+     * hasn't already been scheduled.
+     */
+    @Override
+    public void run() {
+        // Checks if odometry testing is enabled.
+        if (this.enableOdometry.getBoolean(false)) {
+            // Gets the current robot position.
+            Pose2d robotPos = Robot.drive.getRobotPos();
+
+            // Sets the current robot position in the Shuffleboard.
+            this.robotPosition.setDoubleArray(new double[]{robotPos.getX(), robotPos.getY()});
+
+            // Sets the current robot angle in the Shuffleboard.
+            this.robotAngle.setDouble(Robot.drive.getRobotAngle());
+
+            // Gets the current target coords from the Shuffleboard.
+            double targetX = targetPosition.getDoubleArray(new double[]{0.0, 0.0})[0];
+            double targetY = targetPosition.getDoubleArray(new double[]{0.0, 0.0})[1];
+
+            // Gets the command to run for the test.
+            this.command = AutoCommands.driveToPosAutoCommand(targetX, targetY);
+
+            // Schedules the command if not already scheduled.
+            if (!CommandScheduler.getInstance().isScheduled(this.command)) {
+                CommandScheduler.getInstance().schedule(this.command);
+            }
+        }
+        // If not enabled, cancel the current test.
+        else {
+            CommandScheduler.getInstance().cancel(this.command);
+        }
+    }
+}
---
+++ src/main/java/frc/robot/tests/ShooterHoodTest.java
@@ -0,0 +1,119 @@
+package frc.robot.tests;
+
+import edu.wpi.first.wpilibj.shuffleboard.*;
+import edu.wpi.first.util.sendable.Sendable.*;
+import edu.wpi.first.networktables.*;
+import edu.wpi.first.wpilibj2.command.*;
+
+import frc.robot.tests.Test;
+import frc.robot.subsystems.ShooterSystem;
+import frc.robot.commands.AutoCommands;
+import frc.robot.Robot;
+import frc.robot.Constants;
+
+/**
+ * The class for testing the shooter hood in Test mode.
+ */
+public class ShooterHoodTest extends Test {
+    /**
+     * The Shuffleboard tab that the hood 
+     * testing will be displayed on.
+     */
+    private ShuffleboardTab tab;
+
+    /**
+     * The Shuffleboard list that the hood 
+     * testing will be displayed in.
+     */
+    private ShuffleboardLayout layout;
+    
+    /**
+     * The hood angle that the hood is 
+     * theoretically set to (degrees).
+     */
+    private NetworkTableEntry setHoodAngle;
+
+    /**
+     * The hood angle that the hood is
+     * actually set to (degrees).
+     */
+    private NetworkTableEntry actualHoodAngle;
+
+    /**
+     * The current servo offset (degrees).
+     */
+    private NetworkTableEntry currServoOffset;
+
+    /**
+     * The value for whether or not hood
+     * testing is enabled.
+     */
+    private NetworkTableEntry enableHood;
+    
+    /**
+     * The constructor for our shooter hood test.
+     */
+    public ShooterHoodTest() {
+        this.tab = Shuffleboard.getTab("Test");
+        this.layout = tab.getLayout("Shooter Hood Testing").withPosition(4, 0).withSize(2, 4);
+
+        /*
+         * Set the commanded hood angle to the current commanded
+         * hood angle by extracting said commanded hood angle
+         * from the servo offset via subtracting the actual
+         * hood angle from the servo offset.
+         * 
+         * This is done so that when the test is started, the
+         * hood doesn't move immediately to another angle
+         * other than the one it's already at.
+         */
+        this.setHoodAngle = layout.add("Commanded Hood Angle", Robot.shooter.getServoOffset() - Robot.shooter.getHoodAngle()).getEntry();
+        this.actualHoodAngle = layout.add("Actual Hood Angle", Robot.shooter.getHoodAngle()).getEntry();
+        this.currServoOffset = layout.add("Current Servo Offset", Robot.shooter.getServoOffset()).getEntry();
+
+        this.enableHood = layout.add("Enable Hood Test", false).withWidget(BuiltInWidgets.kToggleButton).getEntry();
+    }
+    
+    /**
+     * This method is run every loop of the Test periodic function in the
+     * {@link Robot Robot class}.
+     * <p>
+     * It sets the current set servo offset, it theoretically sets the 
+     * hood to the set hood angle, and it sets the actual hood angle
+     * to the actual hood angle the hood motor was set to.
+     */
+    @Override
+    public void run() {
+        // Checks if hood testing is enabled.
+        if (this.enableHood.getBoolean(false)) {
+            /* 
+             * Sets the servo offset to the servo offset 
+             * in the Shuffleboard.
+             */
+            Robot.shooter.setServoOffset(this.currServoOffset.getDouble(90.0));
+
+            /* 
+             * Sets the hood angle to the commanded hood 
+             * angle in the Shuffleboard.
+             * 
+             * Use Robot.shooter.getServoOffset(), as servo 
+             * offset might be different in Shuffleboard.
+             */
+            Robot.shooter.setHoodAngle(setHoodAngle.getDouble(Robot.shooter.getServoOffset()));
+
+            /* 
+             * Sets the actual hood angle in the Shuffleboard 
+             * to the actual hood angle commanded.
+             */
+            this.actualHoodAngle.setDouble(Robot.shooter.getHoodAngle());
+        }
+        // If not enabled, cancel the current test.
+        else {
+            /* 
+             * Use Robot.shooter.getServoOffset(), as servo offset 
+             * might not be set from Shuffleboard if disabled.
+             */
+            Robot.shooter.setHoodAngle(Robot.shooter.getServoOffset());
+        }
+    }
+}
