cfe5e67a4c1f02e87aa78779f606f3e6ca4e5263
Bigman <Charlieminister@gmail.com>
2022-08-15 11:54:27 -0400

    Started Working on Competition Shuffleboard

---
+++ src/main/java/frc/robot/CompShuffleboard.java
@@ -0,0 +1,16 @@
+package frc.robot;
+
+import java.text.FieldPosition;
+
+import edu.wpi.first.wpilibj.shuffleboard.*;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.networktables.*;
+ 
+
+public class CompShuffleboard {
+    private ShuffleboardTab compTab;
+
+    private NetworkTableEntry field; 
+
+    //this.field = compTab.add("Field", Robot.drive.getField()).withWidget(BuiltInWidgets.kField).getEntry();
+}
