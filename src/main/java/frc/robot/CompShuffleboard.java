b745d052762bbab5b9346216948cba5575c4f7fb
Bigman <Charlieminister@gmail.com>
2022-08-15 15:33:22 -0400

    added Gyro to Shuffleboard

--- src/main/java/frc/robot/CompShuffleboard.java
+++ src/main/java/frc/robot/CompShuffleboard.java
@@ -10,10 +10,12 @@ import edu.wpi.first.networktables.*;
 public class CompShuffleboard {
     private ShuffleboardTab compTab;
 
-    private ComplexWidget field; 
+    private ComplexWidget field;
+    private NetworkTableEntry gyro; 
 
     public CompShuffleboard() {
         this.compTab = Shuffleboard.getTab("Competition");
         this.field = compTab.add("Field", Robot.drive.getField()).withWidget(BuiltInWidgets.kField);
+        this.gyro = compTab.add("Gyro", Robot.drive.getRobotAngle()).withWidget(BuiltInWidgets.kGyro).getEntry();
     }
 }
